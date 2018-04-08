0356b6e830b8997eae751cb844675f5dd663b440
jgabrielgv <jgmezvargas@gmail.com>
2018-04-07 18:45:18 -0600

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyD6oVgGJBF-mCqMF1aLajhS4IjnW_-gWXM",
-    authDomain: "expensify-a6738.firebaseapp.com",
-    databaseURL: "https://expensify-a6738.firebaseio.com",
-    projectId: "expensify-a6738",
-    storageBucket: "expensify-a6738.appspot.com",
-    messagingSenderId: "559667954250"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 
--- src/playground/promises.js
+++ src/playground/promises.js
@@ -12,6 +12,13 @@ console.log('before');
 
 promise.then((data) => {
   console.log(data);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('This is my other promise');
+    }, 3000);
+  });
+}).then((str) => {
+  console.log('Does this run?', str);
 }).catch((error) => {
   console.log(error);
 });
